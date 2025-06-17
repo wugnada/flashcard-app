c32bbec09bfea2a9aad2f350e0c5a872f7faabbc
wugnada <swg1992@gmail.com>
2025-06-17 21:19:14 +0900

    sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,19 @@
-const CACHE='flashcard-v1';self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./'])))});
+const CACHE_NAME = 'text-editor-v1.0.0';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// 설치 이벤트
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('캐시 열림');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
