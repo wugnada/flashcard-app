8b252a5b75c002c47aad28479479c86a7dd09a69
wugnada <swg1992@gmail.com>
2025-06-17 21:19:38 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -17,3 +17,34 @@ self.addEventListener('install', event => {
       })
   );
 });
+
+// 페치 이벤트 (오프라인 지원)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // 캐시에서 발견되면 반환, 아니면 네트워크에서 가져오기
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// 활성화 이벤트 (오래된 캐시 정리)
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('오래된 캐시 삭제:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
