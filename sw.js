2d00521be1a1c398c9a6e016b386b5c2f13bd509
wugnada <swg1992@gmail.com>
2025-06-17 21:28:46 +0900

    sw.js

--- sw.js
+++ sw.js
@@ -1,50 +1,16 @@
-const CACHE_NAME = 'text-editor-v1.0.0';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
+const CACHE_NAME = 'cmd-text-editor-v1';
+const urlsToCache = ['./', './index.html', './manifest.json'];
 
-// 설치 이벤트
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('캐시 열림');
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// 페치 이벤트 (오프라인 지원)
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        // 캐시에서 발견되면 반환, 아니면 네트워크에서 가져오기
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
-
-// 활성화 이벤트 (오래된 캐시 정리)
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('오래된 캐시 삭제:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+      .then(response => response || fetch(event.request))
   );
 });
